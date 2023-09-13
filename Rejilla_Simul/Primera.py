2ad33c3352867495213e624e89ba8f756bee000d
Rafther0112 <r.velasquez@uniandes.edu.co>
2023-09-13 13:50:08 -0500

    actualizacion_simulacion_interactiva

--- Rejilla_Simul/Primera.py
+++ Rejilla_Simul/Primera.py
@@ -3,28 +3,43 @@
 from Aux import apply_kernel, find_path
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
+import numpy as np
+import ipywidgets as widgets
+from ipywidgets import interact
+from IPython.display import display
 
 #Parametros biologicos de densidades
+def simulacion_rejilla(concentracion_crom):
+    densidad_shewanella = 10/10 #Relacionado con la densidad espacial en el fluido
+    densidad_conexiones = 6/8   #Relacionado con la cantidad de tubos de conexion que tiene (completamente biologico)
+    N = 100 #Dimensionalidad de rejilla
+    concentracion_crom = concentracion_crom/100
+    matrix_binaria = np.random.rand(N,N) #Rejilla de simulacion
+    matrix_binaria = (matrix_binaria < densidad_shewanella).astype(int)
 
-densidad_shewanella = 7/10
-densidad_conexiones = 5/10
-densidad_cro = 10/10
-N = 100 #Dimensionalidad de rejilla
+    capacidad_de_conectividad = densidad_conexiones*concentracion_crom
+    nueva_matriz = apply_kernel(matrix_binaria, capacidad_de_conectividad)
 
-matrix_binaria = np.random.rand(N,N) #Rejilla de simulacion
-matrix_binaria = (matrix_binaria < densidad_shewanella).astype(int)
+    nueva_matriz = list(nueva_matriz)
+    camino = find_path(nueva_matriz)
 
 
-nueva_matriz = apply_kernel(matrix_binaria, densidad_conexiones)
+    plt.figure(figsize=(15,15))
+    plt.imshow((-1)*np.array(nueva_matriz), cmap = "viridis_r")
+    if camino: 
+        plt.title(r"Existe camino de medición", fontsize = 24)
+    else: 
+        plt.title(r"No existe camino de medición", fontsize = 24)
+    plt.axis("off")
 
-nueva_matriz = list(nueva_matriz)
-camino = find_path(nueva_matriz)
+# %%
+concentracion_slider = widgets.FloatSlider(
+    value=0.0,
+    min=0.0,
+    max=100,
+    step=0.1,
+    description='Concentracion:')
+interact(simulacion_rejilla, concentracion_crom=concentracion_slider)
 
-if camino: 
-    print("Existe camino de activacion")
-else: 
-    print("No existe camino de conexion")
-plt.figure(figsize=(15,15))
-plt.imshow((-1)*np.array(nueva_matriz), cmap = "viridis_r")
-plt.axis("off")
 # %%
