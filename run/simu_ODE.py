a5d5924f67862c48132333ec2218270f779ac80e
Nicolás Rodríguez García <rodgarnicolas@gmail.com>
2023-09-26 10:18:39 -0500

    update ODE for measuring microplastic

--- run/simu_ODE.py
+++ run/simu_ODE.py
@@ -5,24 +5,24 @@ import pandas as pd
 import os
 
 def model(variables, t, params):
-    p_NahR, C_mNahR, p_LuxI, AHL, p_LuxR, C_LuxRAHL, p_mtrC = variables
-    sigma_pNahR, sigma_rNahR, gamma_rNahR, gamma_pNahR, beta_mNahR, K_mNahR, sigma_pLuxI, gamma_rLuxI, alpha_LuxI, beta_LuxI, K_LuxI, h_LuxI, gamma_pLuxI, eta_pLuxI, beta_LuxRAHL, K_LuxRAKL, sigma_pLuxR, sigma_rLuxR, gamma_rLuxR, gamma_pLuxR, sigma_pmtrC, gamma_rmtrC, alpha_mtrC, beta_mtrC, K_mtrC, h_mtrC, gamma_mtrC = params
+    p_LuxI, p_LuxR, p_mtrC = variables
+    sigma_pNahR, sigma_rNahR, gamma_rNahR, gamma_pNahR, m, K, K1, K2, gamma_Ec, gamma_out, gamma_sh, sigma_pLuxI, gamma_rLuxI, alpha_LuxI, beta_LuxI, K_LuxI, h_LuxI, gamma_pLuxI, beta_LuxRAHL, K_LuxRAHL, sigma_pLuxR, sigma_rLuxR, gamma_rLuxR, gamma_pLuxR, sigma_pmtrC, gamma_rmtrC, alpha_mtrC, beta_mtrC, K_mtrC, h_mtrC, gamma_mtrC = params
     
-    #r_NahR = sigma_rNahR/gamma_rNahR
-    #r_LuxI = (1/gamma_rLuxI)*(alpha_LuxI+(beta_LuxI*C_mNahR/(K_LuxI+C_mNahR**h_LuxI)))
-    #r_luxr = sigma_rLuxR/gamma_rLuxR
-    #r_mtrC = (1/gamma_rmtrC)*(alpha_mtrC+(beta_mtrC*C_LuxRAHL/(h_mtrC + C_LuxRAHL**h_mtrC)))
+    p_NahR = sigma_pNahR*sigma_rNahR/(gamma_rNahR*gamma_pNahR)
+    microNahR = p_NahR*m/(K+m)
+    AHL_sh = sigma_pLuxI*K1*K2/(gamma_Ec + gamma_out*K1 + gamma_sh*K1*K2)
+    luxRAHL = beta_LuxRAHL*AHL_sh/(K_LuxRAHL+AHL_sh)
 
-    dp_Nahrdt = (sigma_pNahR*sigma_rNahR/gamma_rNahR) - gamma_pNahR*p_NahR
-    dC_mNahRdt = 0
-    dmdt = 0
-    dp_LuxIdt = sigma_pLuxI/gamma_rLuxI*(alpha_LuxI+ (beta_LuxI*C_mNahR/(K_LuxI+C_LuxRAHL**h_LuxI))) - gamma_pLuxI*p_LuxI - eta_pLuxI*p_LuxI
-    dAHLdt = 0
-    dp_LuxRdt = sigma_pLuxR*(sigma_rLuxR/gamma_rLuxR) - gamma_pLuxR*p_LuxR
-    dC_luxRAHLdt = 0
-    dp_mtrCdt = (sigma_pmtrC/gamma_rmtrC)*(alpha_mtrC + (beta_mtrC*C_LuxRAHL/(K_mtrC + C_LuxRAHL**h_mtrC))) - gamma_mtrC*p_mtrC
+    dp_LuxIdt = sigma_pLuxI/gamma_rLuxI*(alpha_LuxI + beta_LuxI/(1+ (microNahR/K_LuxI)**-h_LuxI)) - gamma_pLuxI*p_LuxI
+    dp_LuxRdt =  sigma_pLuxR*sigma_rLuxR/gamma_rLuxR - gamma_pLuxR*p_LuxR
+    dp_mtrCdt = sigma_pmtrC/gamma_rmtrC * (alpha_mtrC + beta_mtrC/(1+(luxRAHL/K_mtrC)**(-h_mtrC))) - gamma_mtrC*p_mtrC
 
-    dXdt = [dp_Nahrdt, dC_mNahRdt, dp_LuxIdt, dAHLdt, dp_LuxRdt, dC_luxRAHLdt, dp_mtrCdt]
+    #dp_Nahrdt = (sigma_pNahR*sigma_rNahR/gamma_rNahR) - gamma_pNahR*p_NahR
+    #dp_LuxIdt = sigma_pLuxI/gamma_rLuxI*(alpha_LuxI+ (beta_LuxI*C_mNahR/(K_LuxI+C_LuxRAHL**h_LuxI))) - gamma_pLuxI*p_LuxI - eta_pLuxI*p_LuxI
+    #dp_LuxRdt = sigma_pLuxR*(sigma_rLuxR/gamma_rLuxR) - gamma_pLuxR*p_LuxR
+    #dp_mtrCdt = (sigma_pmtrC/gamma_rmtrC)*(alpha_mtrC + (beta_mtrC*C_LuxRAHL/(K_mtrC + C_LuxRAHL**h_mtrC))) - gamma_mtrC*p_mtrC
+
+    dXdt = [dp_LuxIdt, dp_LuxRdt, dp_mtrCdt]
 
     return dXdt
 
@@ -38,29 +38,21 @@ Tmax = 150
 nums = Tmax*5
 vecTime = np.linspace(0, Tmax, nums)
 
-params_names = ['p_Nahr', 'C_mNahR', 'p_LuxI', 'AHL', '_LuxR', 'C_luxRAHL', 'p_mtrC']
-cond_init = [0.1]*len(params_names)
+variable_names = ['p_LuxI', 'p_LuxR', 'p_mtrC']
+cond_init = [0.1]*len(variable_names)
 
 simu = odeint(model, cond_init, vecTime, args=(params_vals,))
 
-fig, axes = plt.subplots(2,4, figsize=(10,10))
+fig, axes = plt.subplots(1,3, figsize=(10,10))
 
-for idx, name in enumerate(params_names):
-    if idx < 4:
-        axes[0,idx].plot(vecTime, simu[:,idx])
-        axes[0,idx].grid()
-        axes[0,idx].set_ylabel(name)
-    else:
-        axes[1,idx-4].plot(vecTime, simu[:,idx])
-        axes[1,idx-4].grid()
-        axes[1,idx-4].set_ylabel(name)
-        axes[1,idx-4].set_xlabel('Time')
-
-    axes[1,3].get_xaxis().set_visible(False)
-    axes[1,3].get_yaxis().set_visible(False) 
-    axes[1,3].axis('off')
+for idx, name in enumerate(variable_names):
+    axes[idx].plot(vecTime, simu[:,idx])
+    axes[idx].grid()
+    axes[idx].set_ylabel(name)
+    axes[idx].set_xlabel('Time')
 
 plt.tight_layout()
 plt.savefig(os.path.join(main_path, results_path, 'simu_ODE.jpeg'))
 plt.close()
+
 
