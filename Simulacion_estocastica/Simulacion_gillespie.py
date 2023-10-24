a351e3b9492fb95ff41b0f745531b31208518f39
Rafther0112 <r.velasquez@uniandes.edu.co>
2023-10-24 01:46:43 -0500

    updates

--- Simulacion_estocastica/Simulacion_gillespie.py
+++ Simulacion_estocastica/Simulacion_gillespie.py
@@ -4,6 +4,26 @@ import numpy as np
 from tqdm import tqdm
 from numba import jit,njit
 import pandas as pd
+import os
+
+main_path = os.getcwd()
+parameters_path = 'parameters'
+results_path = 'results'
+
+df_params = pd.read_csv(os.path.join('params.csv'))
+
+params_vals = list(df_params['value'])
+variable_names = ['p_LuxI', 'p_mtrC']
+cond_init = [0.1]*len(variable_names)
+
+sigma_pNahR, sigma_rNahR, gamma_rNahR, gamma_pNahR, m, K, K1, K2, gamma_Ec, gamma_out, gamma_sh, sigma_pLuxI, gamma_rLuxI, alpha_LuxI, beta_LuxI, K_LuxI, h_LuxI, gamma_pLuxI, beta_LuxRAHL, K_LuxRAHL, sigma_pLuxR, sigma_rLuxR, gamma_rLuxR, gamma_pLuxR, sigma_pmtrC, gamma_rmtrC, alpha_mtrC, beta_mtrC, K_mtrC, h_mtrC, gamma_pmtrC, V_T, S, E, sigma_AHL = params_vals
+
+v_sh = np.pi*(0.55**2)*2.5
+v_ec = np.pi*(0.5**2)*1.5 
+
+V_sh = S*v_sh
+V_ec = E*v_ec
+constante_grande = (S*K2*K1*sigma_AHL)/(gamma_Ec + gamma_out*K1*((V_T-E*v_ec-S*v_sh)/E*v_ec) + gamma_sh*K2*K1*S*v_sh/E*v_ec)*(v_sh/v_ec)
 
 @njit
 def funcion_creacion_pNahR():
@@ -15,7 +35,7 @@ def funcion_destruccion_pNahR(cantidad_pNahR):
 def funcion_mNahR(cantidad_pNahR, cantidad_m):
     return (cantidad_pNahR*cantidad_m)/(K + cantidad_m)
 
-def funcion_creacion_pLuxI(cantidad_pNahR, cantidad_m)
+def funcion_creacion_pLuxI(cantidad_pNahR, cantidad_m):
     return (sigma_pLuxI/gamma_rLuxI)*(alpha_LuxI + (beta_LuxI)/(1 + (funcion_mNahR(cantidad_pNahR, cantidad_m)/K_LuxI)**(-h_LuxI))) 
 
 def funcion_destruccion_pLuxI(cantidad_pLuxI):
@@ -25,23 +45,25 @@ def cantidad_AHL(cantidad_pLuxI):
     return constante_grande*cantidad_pLuxI
 
 def funcion_creacion_pLuxR():
-    return (sigma_pLuxR*sigma_rLuxR)/gamma_rLuxR
+    return S*(sigma_pLuxR*sigma_rLuxR)/gamma_rLuxR
 
 def funcion_destruccion_pLuxR(cantidad_pLuxR):
     return gamma_pLuxR*cantidad_pLuxR
 
 def funcion_LuxRAHL(cantidad_pLuxR, cantidad_pLuxI):
-    return (S*cantidad_pLuxR*cantidad_AHL(cantidad_pLuxI))/(K_LuxRAHL + cantidad_AHL(cantidad_pLuxI))
+    return (cantidad_pLuxR*cantidad_AHL(cantidad_pLuxI))/(K_LuxRAHL*V_sh + cantidad_AHL(cantidad_pLuxI))
 
-def funcion_creation_pmtrc():
-    return (sigma_pmtrC/gamma_rmtrC)*(alpha_mtrC + (beta_mtrC)/(1 + (funcion_LuxRAHL(cantidad_pLuxR, cantidad_pLuxI)/K_mtrC)**(-h_mtrC))) 
+def funcion_creation_pmtrc(cantidad_pLuxR, cantidad_pLuxI):
+    return (S*sigma_pmtrC/gamma_rmtrC)*(alpha_mtrC + (50*beta_mtrC)/(1 + (funcion_LuxRAHL(cantidad_pLuxR, cantidad_pLuxI)/K_mtrC)**(-h_mtrC))) 
 
 
 def funcion_destruccion_pmtrc(cantidad_pmtrc):
     return gamma_pmtrC*cantidad_pmtrc
+#%%
+print(funcion_LuxRAHL(celulas_prom[:,4][-1], celulas_prom[:,3][-1]))
+#%%
 
-
-@njit
+#@njit
 def modelo_constitutivo(cantidad_pNahR, cantidad_m,cantidad_pLuxI, cantidad_pLuxR, cantidad_pmtrc):
 
     propensidad_creacion_pNahR = funcion_creacion_pNahR()
@@ -53,13 +75,14 @@ def modelo_constitutivo(cantidad_pNahR, cantidad_m,cantidad_pLuxI, cantidad_pLux
     propensidad_creacion_pLuxR = funcion_creacion_pLuxR()
     propensidad_destruccion_pLuxR = funcion_destruccion_pLuxR(cantidad_pLuxR)
 
-    propensidad_creacion_pmtrc = funcion_creation_pmtrc()
+    propensidad_creacion_pmtrc = funcion_creation_pmtrc(cantidad_pLuxR, cantidad_pLuxI)
     propensidad_destruccion_pmtrc = funcion_destruccion_pmtrc(cantidad_pmtrc)
 
 
     return propensidad_creacion_pNahR, propensidad_destruccion_pNahR, propensidad_creacion_pLuxI, propensidad_destruccion_pLuxI , propensidad_creacion_pLuxR, propensidad_destruccion_pLuxR, propensidad_creacion_pmtrc , propensidad_destruccion_pmtrc
 
-@njit('f8[:](f8[:],f8)')
+#@njit('f8[:](f8[:],f8)')
+#@njit
 def Gillespie(trp0,tmax):
     """
     Esta funcion se emplea solamente para hacer la evolución de un paso individual en la celula. Evoluciona no un paso temporal, 
@@ -69,7 +92,7 @@ def Gillespie(trp0,tmax):
     t, cantidad_pNahR, cantidad_m, cantidad_pLuxI, cantidad_pLuxR, cantidad_pmtrc =trp0 
 
     while t < tmax:
-        s_1, s_2, s_3, s_4, s_5, s_6, s_7, s_8 = modelo_constitutivo(cantidad_pNahR, cantidad_m,cantidad_pLuxI, cantidad_pLuxR, cantidad_pmtrc):
+        s_1, s_2, s_3, s_4, s_5, s_6, s_7, s_8 = modelo_constitutivo(cantidad_pNahR, cantidad_m,cantidad_pLuxI, cantidad_pLuxR, cantidad_pmtrc)
         S_T = s_1 + s_2 + s_3 + s_4 + s_5 + s_6 + s_7 + s_8 
 
         τ = (-1/S_T)*np.log(np.random.rand())
@@ -102,7 +125,8 @@ def Gillespie(trp0,tmax):
         t+=τ
     return np.array([t, cantidad_pNahR, cantidad_m, cantidad_pLuxI, cantidad_pLuxR, cantidad_pmtrc]) 
 
-@njit('f8[:,:](f8[:],f8[:])')
+#@njit('f8[:,:](f8[:],f8[:])')
+#@njit
 def Estado_celula(X0,tiempos):
 
     X = np.zeros((len(tiempos),len(X0)))
@@ -112,10 +136,18 @@ def Estado_celula(X0,tiempos):
         X[i] = Gillespie(X[i-1],tiempos[i])
     
     return X
-
+#%%
 x0 = np.array([0., 0., 0., 0., 0., 0.])
 
-num_cel = 1000 #número de células 
+num_cel = 10 #número de células 
 celulas = np.array([Estado_celula(x0,np.arange(0.,700.,2.)) for i in tqdm(range(num_cel))])
 
 celulas_prom = np.mean(celulas,axis=0) #axis = 0 saca el promedio componente a componente de cada célula.
+#%%
+import matplotlib.pyplot as plt
+plt.plot(celulas_prom[:,5])
+# %%
+print(len(celulas_prom[:,0]))
+# %%
+constante_grande
+# %%
---
+++ Solucion_determinista/simu_ODE.py
@@ -0,0 +1,81 @@
+#%%
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.integrate import odeint
+import pandas as pd
+import os
+
+def model_biosensor(variables, t, params):
+    p_LuxI, p_mtrC = variables
+    sigma_pNahR, sigma_rNahR, gamma_rNahR, gamma_pNahR, m, K, K1, K2, gamma_Ec, gamma_out, gamma_sh, sigma_pLuxI, gamma_rLuxI, alpha_LuxI, beta_LuxI, K_LuxI, h_LuxI, gamma_pLuxI, beta_LuxRAHL, K_LuxRAHL, sigma_pLuxR, sigma_rLuxR, gamma_rLuxR, gamma_pLuxR, sigma_pmtrC, gamma_rmtrC, alpha_mtrC, beta_mtrC, K_mtrC, h_mtrC, gamma_mtrC, V_T, S, E, sigma_AHL = params
+    
+    '''
+    E = numero de E. coli
+    S = numero de S. oneidensis
+    v_ec = volumen de 1 E. coli
+    v_sh = volumen de 1 S. oneidensis
+    V_ec = E * v_ec
+    V_sh = S * v_sh
+    '''
+
+    #los tamaños estan en micrometros (10^-6)
+    v_sh = np.pi*(0.55**2)*2.5
+    v_ec = np.pi*(0.5**2)*1.5 
+
+    V_sh = S*v_sh
+    V_ec = E*v_ec
+
+    #V_out = V_T - V_ec - V_sh
+    
+    p_NahR = (sigma_pNahR*sigma_rNahR)/(gamma_rNahR*gamma_pNahR)
+    microNahR = (p_NahR*m/(K+m))
+    AHL_sh = (S*K2*K1*sigma_AHL*p_LuxI)/(gamma_Ec + gamma_out*K1*((V_T-E*v_ec-S*v_sh)/E*v_ec) + gamma_sh*K2*K1*S*v_sh/E*v_ec)*(v_sh/v_ec)
+    p_LuxR = S*sigma_pLuxR*sigma_rLuxR/(gamma_pLuxR*gamma_rLuxR)
+    luxRAHL = (p_LuxR*AHL_sh)/((K_LuxRAHL*V_sh)+AHL_sh)
+    #HAY QUE REVISAR BIEN ALPHA Y BETA PORQUE POR ESO ESTA DANDO TAN POQUITO
+    dp_LuxIdt = sigma_pLuxI/gamma_rLuxI*(alpha_LuxI + beta_LuxI/(1+ (microNahR/K_LuxI)**-h_LuxI)) - gamma_pLuxI*p_LuxI
+    dp_mtrCdt = S*sigma_pmtrC/gamma_rmtrC * ((alpha_mtrC) + (beta_mtrC)/(1+(luxRAHL/K_mtrC)**(-h_mtrC))) - gamma_mtrC*p_mtrC
+
+    
+    dXdt = [dp_LuxIdt, dp_mtrCdt]
+
+    return dXdt
+
+main_path = os.getcwd()
+parameters_path = 'parameters'
+results_path = 'results'
+
+if os.path.exists(os.path.join(main_path,results_path)) == False:
+    os.mkdir(os.path.join(main_path, results_path))
+
+if os.path.exists(os.path.join(main_path, parameters_path)) == False:
+    os.mkdir(os.path.join(main_path, parameters_path))
+
+df_params = pd.read_csv(os.path.join('params.csv'))
+
+params_vals = list(df_params['value'])
+
+Tmax = 800
+nums = Tmax*5
+vecTime = np.linspace(0, Tmax, nums)
+
+variable_names = ['p_LuxI', 'p_mtrC']
+cond_init = [0.1]*len(variable_names)
+
+simu = odeint(model_biosensor, cond_init, vecTime, args=(params_vals,))
+
+fig, axes = plt.subplots(1,2, figsize=(9,6))
+titulos = ["p_LuxI vs time", "p_mtrC vs time"]
+for idx, name in enumerate(variable_names):
+    axes[idx].set_title(fr"{titulos[idx]}")
+    axes[idx].plot(vecTime, simu[:,idx])
+
+    axes[idx].set_ylabel(fr"{name}")
+    axes[idx].set_xlabel(r'Time (min)')
+
+plt.tight_layout()
+plt.savefig('simu_ODE.jpeg')
+plt.close()
+
+
+# %%
