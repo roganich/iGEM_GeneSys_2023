c87b9423820807c686304faf0ba8379d986488ae
Rafther0112 <r.velasquez@uniandes.edu.co>
2023-10-30 00:33:28 -0500

    updates

--- Rejilla_Simul/Aux.py
+++ Rejilla_Simul/Aux.py
@@ -19,60 +19,7 @@ def apply_kernel(matrix, densidad_tubulinas):
                 result[i * 3:(i + 1) * 3, j * 3:(j + 1) * 3] = kernel
     
     return result
-"""
-def find_path(matrix):
-    if not matrix:
-        return None
 
-    rows, cols = len(matrix), len(matrix[0])
-
-    def is_valid(x, y):
-        return 0 <= x < rows and 0 <= y < cols and matrix[x][y] == 1
-
-    moves = [(1, 0), (-1, 0), (0, 1), (0, -1), (1, 1), (-1, -1), (1, -1), (-1, 1)]
-
-    # Bordes derecha e izquierda
-#    for i in range(rows):
-#        if matrix[i][0] == 1:
-#            # Comenzar una búsqueda desde el borde izquierdo
-#            queue = deque([(i, 0)])
-#            visited = set([(i, 0)])
-
-#            while queue:
-#                x, y = queue.popleft()
-
-#                if y == cols - 1:
-#                    return True  # Se encontró un camino hacia el borde derecho
-
-                # Explorar las celdas vecinas 
-#                for dx, dy in moves:
-#                    nx, ny = x + dx, y + dy
-#                    if is_valid(nx, ny) and (nx, ny) not in visited:
-#                        queue.append((nx, ny))
-#                        visited.add((nx, ny))
-
-    # Bordes superior e inferior
-    for j in range(cols):
-        if matrix[0][j] == 1:
-            # Comenzar una búsqueda desde el borde superior
-            queue = deque([(0, j)])
-            visited = set([(0, j)])
-
-            while queue:
-                x, y = queue.popleft()
-
-                if x == rows - 1:
-                    return True  # Se encontró un camino hacia el borde inferior
-
-                # Explorar las celdas vecinas 
-                for dx, dy in moves:
-                    nx, ny = x + dx, y + dy
-                    if is_valid(nx, ny) and (nx, ny) not in visited:
-                        queue.append((nx, ny))
-                        visited.add((nx, ny))
-
-    return False
-""" 
 def find_path(matrix):
     if not matrix:
         return None
@@ -105,5 +52,4 @@ def find_path(matrix):
                     if is_valid(nx, ny) and (nx, ny) not in visited:
                         queue.append((nx, ny))
                         visited.add((nx, ny))
-
     return None
